dac255df77e8e84e5c87bfaef82ee92c3a1bf3c5
volodko.ura <volodko_ura@mail.ru>
2021-06-04 16:46:16 +0300

    Initial commit

---
+++ Masseges/Chat.py
@@ -0,0 +1,32 @@
+from MyUser import User
+name = input("Введите имя пользователя чата: ")
+user = User(name, "http://127.0.0.1:50550/")
+
+while True:
+    print("Программа чат! Выберите действие, которое хотите выполнить: ")
+    print("="*50)
+    menu = input("1. Войти в чат.   2. Выйти из пограммы! ")
+    if menu == "1":
+        print("Вы вошли в чат!")
+        print("-"*50)
+        user.post_login()
+        print("Программа чат! Для выхода нажмите 0!!! ")
+        while True:
+            print("-"*50)
+            mes = input("Введите сообщение: ")
+            user.send_massege(mes)
+            user.get_massege()
+            if mes == "0":
+                print("Вы вышли из чата!")
+                break
+
+    elif menu == "2":
+        break
+    else:
+        print("Введены не коректные данные! Попробуйте снова!")
+
+
+
+
+
+
---
+++ Masseges/MyUser.py
@@ -0,0 +1,35 @@
+import requests
+import json
+class User:
+
+    nickname: str
+    url: str = ""
+    massege: str
+
+    def __init__(self, nickname, url):
+        self.nickname = nickname
+        self.url = url
+        self.session = requests.session()
+
+
+    def post_login(self):
+        response = self.session.post(self.url + '/login', json={"u_name": self.nickname})
+        return f"{response.status_code} - {response.text}"
+
+    def get_massege(self):
+        response = self.session.get(self.url + '/get_messages')
+
+        for value in json.loads(response.text).values():
+            for val in value:
+                #print(val)
+                for key, value in val.items():
+                    print(f"{key}: {value}")
+
+    def send_massege(self, massage):
+        response = self.session.post(self.url + '/send_message', json= {self.nickname: massage})
+        #print(response.status_code)
+        return f"{response.status_code} - {response.text}"
+
+    def post_logout(self):
+        response = self.session.post(self.url + '/logout', json= {"u_name": self.nickname})
+        return f"{response.status_code} - {response.text}"
